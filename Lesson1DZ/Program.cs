7a0d88f843073997021e825588d5e662ecb6c4ad
Poli_tn62@student.itstep.kz <Poli_tn62@student.itstep.kz>
2020-12-04 23:40:01 +0600

    Добавьте файлы проекта.

---
+++ Lesson1DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson1DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Igor Polivin");
+            int num1, num2;
+            Console.WriteLine("Enter first number: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Summa = {num1 + num2 }");
+        }
+    }
+}
